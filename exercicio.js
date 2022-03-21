6e4feb854c4b10302c9695103840022d19e7df8e
willian <willian.chris.89@gmail.com>
2022-03-21 15:23:54 -0300

    exercício sendo refeito do 0

--- exercicio.js
+++ exercicio.js
@@ -2,19 +2,60 @@
  O valor unitário de cada energético é fixo e cada um custa R$ 4,50.
   As alíquotas de imposto que serão somados no valor são os seguintes: ICMS: 18% IPI: 4% PIS: 1,86% COFINS: 8,54% */
 function energetico(quantidade) {
-  const icms = 18 //18% de algo 
-  const ipi = 4
-  const pis = 1.86
-  const cofins = 8.54
-  const valorUnitario = 4.50
-  const somaPorcentagem = icms + ipi + pis + cofins
-
-  const soma = valorUnitario * quantidade
-  console.log(soma)
-  //const porcentagem =  soma*ipi
+const icms = 18  / 100
+const ipi = 4 / 100
+const pis = 1.86  / 100
+const cofins =8.54  / 100
+const valorUnitario = 4.50
+const valorBruto = quantidade * valorUnitario
+console.log(valorBruto)
+ const impostoDeIcms = icms * valorBruto
+ const impostoDeIpi = ipi * valorBruto
+ const impostoDePis = pis * valorBruto
+ const impostoDeCofins = cofins * valorBruto
+ const somaTotal = impostoDeIcms + impostoDeIpi + impostoDePis + impostoDeCofins + valorBruto
+ console.log(somaTotal)
+//  console.log(impostoDeIcms)
+}
+ energetico(1000)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  // console.log(quantidade/2)
+//   const icms = 18 / 100 //18% de algo 
+//   const ipi = 4 / 100
+//   const pis = 1.86 / 100
+//   const cofins = 8.54 / 100
+//   const valorUnitario = 4.50
+//   const somaPorcentagem = icms + ipi + pis + cofins
+// console.log(somaPorcentagem)
+//   const soma = (valorUnitario * quantidade + somaPorcentagem).toFixed(1)
+// console.log(soma)
+//   const porcentagem = soma * icms
+//   const porcentagem2 = soma * ipi
+//   const porcentagem3 = soma * pis
+//   const porcentagem4 = soma * cofins
+//   const somadorDePorcentagem = (porcentagem + porcentagem2 + porcentagem3 + porcentagem4).toFixed()
+  // console.log(somadorDePorcentagem)
+  // const somaTotal = somaPorcentagem + quantidade
+  // console.log(somaTotal)
   //console.log(porcentagem.toFixed())
   //const porcentagemSoma = soma /100 
   /* console.log(porcentagemSoma) */
-  return `Cliente:Supermercado Dosul ICMS: ${(soma * icms).toFixed(1)} IPI:R$${(soma * ipi).toFixed(1)}; PIS: R$${(soma * pis).toFixed(1)}; COFINS: R$${(soma * cofins).toFixed(1)}; TOTAL: R$${soma}`
-}
-console.log(energetico(500))
+//   return `Cliente:Supermercado Dosul ICMS:R$${porcentagem.toFixed()} IPI:R$${porcentagem2.toFixed()}; PIS: R$${porcentagem3.toFixed()}; COFINS: R$${porcentagem4.toFixed()}; TOTAL: R$${soma}`
+// }
+// console.log(energetico(500))
